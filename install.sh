fd5ee1157161d0e9d1511a98740131a06edba2bc
Ramón Isijara <risijara@gmail.com>
2017-06-07 16:03:54 -0500

    Corrección de ruta de archivos temporales

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 cp .vimrc ~/.vimrc
 git clone https://github.com/flazz/vim-colorschemes.git ~/my-vim-colors-tmp
-mv my-vim-colors-tmp/colors ~/.vim/colors
+mv ~/my-vim-colors-tmp/colors ~/.vim/colors
 rm -rf ~/my-vim-colors-tmp
