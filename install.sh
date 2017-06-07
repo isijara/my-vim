61ebe48c7f9e128bbb869682b7e61b822808da21
Ramón Isijara <risijara@gmail.com>
2017-06-07 16:04:25 -0500

    Agregado Vundle a script de instalación

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
 git clone https://github.com/flazz/vim-colorschemes.git ~/my-vim-colors-tmp
 mv ~/my-vim-colors-tmp/colors ~/.vim/colors
